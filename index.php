81ccc34ab8c26d4caea451c2485efbc2d78df62e
Elton Gomes <eltongomes8013@gmail.com>
2021-09-17 09:16:14 -0300

    adicionando idade

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ $categorias[] = 'adulto';
 
 //print_r($categorias);
 
-$nome = 'Elisa';
-$idade = 21;
+$nome = 'Luiz';
+$idade = 16;
 
 //var_dump($nome);
 //var_dump($idade);
@@ -17,7 +17,7 @@ if($idade <= 12)
     for($i = 0; $i <= count($categorias); $i++)
     {
         if($categorias[$i] == 'infantil')
-            echo 'O nadador '.$nome. ' compete na categoria infantil';
+            echo 'O nadador '.$nome. ' compete na categoria infantil e tem ' .$idade.' anos.';
         }
         
 }
@@ -26,7 +26,7 @@ elseif($idade >= 13 && $idade <= 18)
 for($i = 0; $i <= count($categorias); $i++)
     {
         if($categorias[$i] == 'adolescente')
-            echo 'O nadador '.$nome. ' compete na categoria adolescente';
+            echo 'O nadador '.$nome. ' compete na categoria adolescente e tem '.$idade.' anos.';
         }
    
 }
@@ -35,6 +35,6 @@ else
     for($i = 0; $i <= count($categorias); $i++)
     {
         if($categorias[$i] == 'adulto')
-            echo 'O nadador '.$nome. ' compete na categoria adulto';
+            echo 'O nadador '.$nome. ' compete na categoria adulto e tem ' .$idade.' anos.';
         }
 }
